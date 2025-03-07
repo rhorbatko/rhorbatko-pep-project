7000da9001b727b3dfc6459d645471dbb249b6f9
Roman Horbatko <rghorbatko@gmail.com>
2025-03-07 03:25:28 +0000

    created insert user and get user methods in UserDAO

---
+++ src/main/java/DAO/UserDAO.java
@@ -0,0 +1,60 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+public class UserDAO {
+
+
+    // register user
+    public Account insertUser(Account account){
+        Connection conn = ConnectionUtil.getConnection();
+        try{
+            String sql = "INSERT INTO account (username, password) VALUES (?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, account.getUsername());
+            ps.setString(2, account.getPassword());
+            ps.executeUpdate();
+            ResultSet pkResultSet = ps.getGeneratedKeys();
+            if (pkResultSet.next()){
+                int generated_account_id = (int) pkResultSet.getLong(1);
+                account.setAccount_id(generated_account_id);
+                return account;
+            }
+
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    };
+    //retrieve user
+    public Account getUser(String username, String password){
+        Connection conn = ConnectionUtil.getConnection();
+        try {
+            String sql = "SELECT * FROM account WHERE username = ? AND password = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                Account acc = new Account(
+                rs.getString("username"),
+                rs.getString("password"));
+                acc.setAccount_id(rs.getInt("account_id"));
+                return acc; 
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+        
+    }
+}
